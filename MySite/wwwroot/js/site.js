793acd9cd89405178825aaf31edfb87548e59846
Ferrario-Filippo <f.filippo49@gmail.com>
2022-05-15 14:18:41 +0200

    Add project files.

---
+++ MySite/wwwroot/js/site.js
@@ -0,0 +1,21 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+"use-strict"
+
+function coding_clicked() {
+    alert("C#");
+}
+
+function cooking_clicked() {
+    alert("Cookie");
+}
+
+function travel_clicked() {
+    alert("Viaggi");
+}
+
+function mind_clicked() {
+    alert("Mind")
+}
